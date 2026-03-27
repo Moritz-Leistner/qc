076cf851c8244a5874696cf716e2bed61bcabcdf
Moritz-Leistner <moritz.leistner@gmail.com>
2026-03-27 22:33:28 +0100

    chore(agx_utils): add stratified sampling

--- envs/agx_utils.py
+++ envs/agx_utils.py
@@ -15,11 +15,77 @@ import agxcave.agxtasks.excavator.rock_capturing.config.rock_capturing_cfg as ag
 BASE = "agxcave.agxtasks.excavator"
 ROCK_CONFIG = f"{BASE}.rock_capturing.config"
 
+negative_files = [
+"demonstration_2026-01-18_18:11:01.pkl",
+"demonstration_2026-01-18_18:11:08.pkl",
+"demonstration_2026-01-18_18:23:53.pkl",
+"demonstration_2026-01-18_18:40:19.pkl",
+"demonstration_2026-01-18_18:11:03.pkl",
+"demonstration_2026-01-18_18:21:00.pkl",
+"demonstration_2026-01-18_18:39:43.pkl",
+"demonstration_2026-01-18_18:47:35.pkl",
+"demonstration_2026-01-18_18:40:01.pkl",
+"demonstration_2026-01-18_18:48:54.pkl",
+"demonstration_2026-01-18_18:11:06.pkl",
+"demonstration_2026-01-18_18:44:10.pkl",
+"demonstration_2026-01-18_18:49:20.pkl",
+"demonstration_2026-01-18_18:30:36.pkl",
+"demonstration_2026-01-18_18:31:17.pkl",
+"demonstration_2026-01-18_18:23:50.pkl",
+"demonstration_2026-01-18_18:31:35.pkl",
+"demonstration_2026-01-18_18:52:07.pkl",
+"demonstration_2026-01-18_18:10:58.pkl",
+"demonstration_2026-01-18_18:44:28.pkl",
+"demonstration_2026-01-18_18:23:56.pkl"
+]
 
-def load_demo_pickles(demo_dir):
+
+positive_files = ['demonstration_2026-01-18_18:11:48.pkl', 'demonstration_2026-01-18_18:12:15.pkl', 'demonstration_2026-01-18_18:12:47.pkl', 'demonstration_2026-01-18_18:13:12.pkl', 
+'demonstration_2026-01-18_18:13:35.pkl', 'demonstration_2026-01-18_18:14:04.pkl', 'demonstration_2026-01-18_18:14:27.pkl', 'demonstration_2026-01-18_18:15:22.pkl', 'demonstration_2026-01-18_18:15:44.pkl',
+ 'demonstration_2026-01-18_18:16:06.pkl', 'demonstration_2026-01-18_18:16:31.pkl', 'demonstration_2026-01-18_18:16:55.pkl', 'demonstration_2026-01-18_18:17:13.pkl', 'demonstration_2026-01-18_18:17:41.pkl', 
+ 'demonstration_2026-01-18_18:18:13.pkl', 'demonstration_2026-01-18_18:18:33.pkl', 'demonstration_2026-01-18_18:19:06.pkl', 'demonstration_2026-01-18_18:19:34.pkl', 'demonstration_2026-01-18_18:20:01.pkl', 
+ 'demonstration_2026-01-18_18:20:20.pkl', 'demonstration_2026-01-18_18:20:48.pkl', 'demonstration_2026-01-18_18:21:21.pkl', 'demonstration_2026-01-18_18:21:44.pkl', 'demonstration_2026-01-18_18:22:05.pkl', 
+ 'demonstration_2026-01-18_18:22:24.pkl', 'demonstration_2026-01-18_18:22:41.pkl', 'demonstration_2026-01-18_18:22:59.pkl', 'demonstration_2026-01-18_18:23:17.pkl', 'demonstration_2026-01-18_18:23:42.pkl', 
+ 'demonstration_2026-01-18_18:24:48.pkl', 'demonstration_2026-01-18_18:25:10.pkl', 'demonstration_2026-01-18_18:26:07.pkl', 'demonstration_2026-01-18_18:26:43.pkl', 'demonstration_2026-01-18_18:27:04.pkl', 
+ 'demonstration_2026-01-18_18:27:21.pkl', 'demonstration_2026-01-18_18:27:40.pkl', 'demonstration_2026-01-18_18:27:56.pkl', 'demonstration_2026-01-18_18:28:13.pkl', 'demonstration_2026-01-18_18:28:58.pkl', 
+ 'demonstration_2026-01-18_18:29:34.pkl', 'demonstration_2026-01-18_18:30:10.pkl', 'demonstration_2026-01-18_18:31:03.pkl', 'demonstration_2026-01-18_18:31:49.pkl', 'demonstration_2026-01-18_18:32:12.pkl', 
+ 'demonstration_2026-01-18_18:32:38.pkl', 'demonstration_2026-01-18_18:33:02.pkl', 'demonstration_2026-01-18_18:33:23.pkl', 'demonstration_2026-01-18_18:33:42.pkl', 'demonstration_2026-01-18_18:34:16.pkl', 
+ 'demonstration_2026-01-18_18:34:51.pkl', 'demonstration_2026-01-18_18:35:12.pkl', 'demonstration_2026-01-18_18:35:41.pkl', 'demonstration_2026-01-18_18:36:00.pkl', 'demonstration_2026-01-18_18:36:30.pkl', 
+ 'demonstration_2026-01-18_18:36:52.pkl', 'demonstration_2026-01-18_18:37:24.pkl', 'demonstration_2026-01-18_18:37:46.pkl', 'demonstration_2026-01-18_18:38:09.pkl', 'demonstration_2026-01-18_18:38:30.pkl', 
+ 'demonstration_2026-01-18_18:38:51.pkl', 'demonstration_2026-01-18_18:39:15.pkl', 'demonstration_2026-01-18_18:39:31.pkl', 'demonstration_2026-01-18_18:40:41.pkl', 'demonstration_2026-01-18_18:41:09.pkl', 
+ 'demonstration_2026-01-18_18:41:45.pkl', 'demonstration_2026-01-18_18:42:05.pkl', 'demonstration_2026-01-18_18:42:26.pkl', 'demonstration_2026-01-18_18:43:17.pkl', 'demonstration_2026-01-18_18:43:36.pkl', 
+ 'demonstration_2026-01-18_18:43:51.pkl', 'demonstration_2026-01-18_18:44:53.pkl', 'demonstration_2026-01-18_18:45:08.pkl', 'demonstration_2026-01-18_18:45:26.pkl', 'demonstration_2026-01-18_18:45:45.pkl', 
+ 'demonstration_2026-01-18_18:46:01.pkl', 'demonstration_2026-01-18_18:46:22.pkl', 'demonstration_2026-01-18_18:46:40.pkl', 'demonstration_2026-01-18_18:46:55.pkl', 'demonstration_2026-01-18_18:47:09.pkl', 
+ 'demonstration_2026-01-18_18:49:34.pkl', 'demonstration_2026-01-18_18:49:50.pkl', 'demonstration_2026-01-18_18:50:12.pkl', 'demonstration_2026-01-18_18:50:28.pkl', 'demonstration_2026-01-18_18:50:47.pkl', 
+ 'demonstration_2026-01-18_18:51:19.pkl', 'demonstration_2026-01-18_18:51:37.pkl', 'demonstration_2026-01-18_18:52:24.pkl', 'demonstration_2026-01-18_18:52:49.pkl', 'demonstration_2026-01-18_18:53:17.pkl']
+
+
+def load_demo_pickles(demo_dir, n_samples, seed=None):
+
+    if seed is not None:
+        random.seed(seed)
+
+    pos_files = positive_files
+    neg_files = negative_files
+
+    total = len(pos_files) + len(neg_files)
+    pos_ratio = len(pos_files) / total
+
+    n_pos = round(n_samples * pos_ratio)
+    n_neg = n_samples - n_pos
+
+    n_pos = min(n_pos, len(pos_files))
+    n_neg = min(n_neg, len(neg_files))
+
+    sampled_pos = random.sample(pos_files, n_pos)
+    sampled_neg = random.sample(neg_files, n_neg)
+
+    sampled_files = sampled_pos + sampled_neg
+    random.shuffle(sampled_files)
+
     demos = []
 
-    for fname in sorted(os.listdir(demo_dir)):
+    for fname in sampled_files:
         if not fname.endswith(".pkl"):
             continue
         with open(os.path.join(demo_dir, fname), "rb") as f:
@@ -28,13 +94,7 @@ def load_demo_pickles(demo_dir):
     return demos
 
 
-def demos_to_dataset(demos, reward_type, wrapper, enable_vision, max_demos=None, seed=None):
-
-    rng = random.Random(seed)
-    rng.shuffle(demos)
-
-    if max_demos is not None:
-        demos = demos[:max_demos]
+def demos_to_dataset(demos, reward_type, wrapper, enable_vision):
 
     obs, actions, rewards, terminals, next_obs = [], [], [], [], []
 
@@ -120,8 +180,8 @@ def make_agx_env_and_dataset(env_name, demo_dir, reward, enable_vision=False, ma
     # Since agx prohibits us from running two siimulations on the same thread we reuse the training env for eval
     eval_env = None
 
-    demos = load_demo_pickles(demo_dir)
-    train_dataset = demos_to_dataset(demos, reward, env, enable_vision, max_demos, seed)
+    demos = load_demo_pickles(demo_dir, max_demos, seed)
+    train_dataset = demos_to_dataset(demos, reward, env, enable_vision)
 
     return env, env, train_dataset, None
 
